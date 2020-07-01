be9117f4cf398c010d29e260195ba510db7a6e03
Aviskar KC <aviskarkc10@gmail.com>
2020-07-01 20:23:08 +0545

    Allow user to copy commit messages to clipboard

--- index.js
+++ index.js
@@ -8,7 +8,48 @@ const copyCommitBtn = document.createElement('button');
 
 copyCommitBtn.type = 'button';
 copyCommitBtn.className = 'btn btn-secondary';
-copyCommitBtn.style.margin = '0px 5px';
+copyCommitBtn.style.margin = '0px 10px';
 copyCommitBtn.innerHTML = 'Copy commits';
 
 btnContainer.insertBefore(copyCommitBtn, btnContainer.firstChild);
+
+const handleButtonClick = () => {
+  const commitMessageContainers = document.getElementsByClassName(
+    'commit-message'
+  );
+
+  let commitMessages = ``;
+
+  commitMessageContainers &&
+    Object.keys(commitMessageContainers).forEach((key) => {
+      const commitMessage =
+        commitMessageContainers[key].querySelector('code a').title || '';
+
+      commitMessages += `- ${commitMessage}\n`;
+    });
+
+  console.log(commitMessages);
+
+  copyCommitsToClipBoard(commitMessages);
+
+  copyCommitBtn.innerHTML = 'Copied to clipboard!';
+
+  setTimeout(() => {
+    copyCommitBtn.innerHTML = 'Copy commits';
+  }, 3000);
+};
+
+const copyCommitsToClipBoard = (commitMessages) => {
+  const el = document.createElement('textarea');
+
+  el.value = commitMessages;
+
+  document.body.appendChild(el);
+
+  el.select();
+
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+btnContainer.addEventListener('click', handleButtonClick);
